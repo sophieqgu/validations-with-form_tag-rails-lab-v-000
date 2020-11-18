91d6b72f12520e6f60de1f90292b8c459d67a108
Sophie Gu <sophieqgu@gmail.com>
2020-11-18 04:59:22 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,12 +23,11 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.valid?
-      @post.update(post_params)
+    if @post.update(post_params)
       redirect_to post_path(@post)
     else
       render :edit
-    end 
+    end
   end
 
   private
