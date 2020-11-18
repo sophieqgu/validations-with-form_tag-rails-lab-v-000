5061b72734fff96c0290c7d8dc5641cbc87fc56d
Sophie Gu <sophieqgu@gmail.com>
2020-11-18 04:25:10 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, 
+  validates :category, inclusion: { in: %w(fiction, non-fiction) }
 end
